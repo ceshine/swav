b9297aa5ccb00bdbe8742a5d6fc50ebc6d2caff4
Ceshine Lee <shuanck@gmail.com>
2020-08-03 10:36:47 +0800

    Update training parameters

--- scripts/swav_bit_helperbot.sh
+++ scripts/swav_bit_helperbot.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/fish
 set -x DATASET_PATH "/data/pretrain_dataset/"
-set -x EXPERIMENT_PATH "cache/swav/swav_bit50_helperbot"
+set -x EXPERIMENT_PATH "cache/swav/swav_bit50_helperbot_e5"
 mkdir -p $EXPERIMENT_PATH
 
 python -u main_swav_bit_helperbot.py \
 --data_path $DATASET_PATH \
---nmb_crops 2 6 \
---size_crops 256 128 \
---min_scale_crops 0.25 0.10 \
---max_scale_crops 1. 0.25 \
+--nmb_crops 2 4 \
+--size_crops 256 96 \
+--min_scale_crops 0.35 0.10 \
+--max_scale_crops 1. 0.35 \
 --crops_for_assign 0 1 \
 --temperature 0.1 \
 --epsilon 0.05 \
@@ -16,14 +16,17 @@ python -u main_swav_bit_helperbot.py \
 --feat_dim 128 \
 --nmb_prototypes 1000 \
 --queue_length 2560 \
---epochs 4 \
---batch_size 16 \
---base_lr 0.0003 \
+--epochs 5 \
+--batch_size 24 \
+--base_lr 0.00008 \
 --freeze_prototypes_niters 500 \
---wd 0 \
+--wd 0. \
 --warmup_epochs .5 \
 --arch BiT-M-R50x1 \
 --pretrained_path cache/pretrained/ \
 --use_fp16 true \
 --dump_path $EXPERIMENT_PATH \
---checkpoint_freq 5000
+--checkpoint_freq 5000 \
+--grad_accu 4 \
+--hidden_mlp 1028 \
+--seed 42
